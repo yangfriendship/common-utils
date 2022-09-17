f07afa2f845928e124e69e1b699b0d13a520f9ce
woojung yang <yangfriendship@byulsoft.com>
2022-09-17 17:23:43 +0900

    add: isSame 기능 추가 및 string parse 기능 추가

--- src/main/java/me/youzheng/common/timeutil/TimeUtil.java
+++ src/main/java/me/youzheng/common/timeutil/TimeUtil.java
@@ -3,10 +3,24 @@ package me.youzheng.common.timeutil;
 import lombok.experimental.UtilityClass;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiPredicate;
 
 @UtilityClass
 public class TimeUtil {
+
+    private static final Map<String, DateTimeFormatter> dateTimeFormatterCache = new ConcurrentHashMap<>();
+    private static Locale DEFAULT_LOCALE = Locale.getDefault();
+    private static WeekFields WEEK_FIELDS = WeekFields.of(DEFAULT_LOCALE);
+
+    public static void setLocale(Locale locale) {
+        DEFAULT_LOCALE = locale;
+        WEEK_FIELDS= WeekFields.of(locale);
+    }
 
     public static boolean match(LocalDateTime source, TimePredicate timePredicate) {
         return timePredicate.getPredicate().test(source, timePredicate.getTarget());
@@ -33,6 +47,10 @@ public class TimeUtil {
         return IsSameYear.create(target);
     }
 
+    public static TimePredicate isSameMonth(LocalDateTime target) {
+        return IsSameMonth.create(target);
+    }
+
     public static TimePredicate isSameWeek(LocalDateTime target) {
         return IsSameWeek.create(target);
     }
@@ -45,32 +63,71 @@ public class TimeUtil {
         return IsSameMin.create(target);
     }
 
-    public static TimePredicate isAfterWeekOf(long weeks, LocalDateTime target) {
-        return IsSameWeek.create(target.plusWeeks(weeks));
-    }
-
-    public static TimePredicate isAfterDaysOf(long days, LocalDateTime target) {
-        return IsSameWeek.create(target.plusDays(days));
-    }
-
     public static TimePredicate[] isBetween(LocalDateTime start, LocalDateTime end) {
-        return new TimePredicate[]{IsAfter.create(start), IsBefore.create(end)};
+        LocalDateTime from;
+        LocalDateTime to;
+        if (start.isBefore(end)) {
+            from = start;
+            to = end;
+        } else {
+            from = end;
+            to = start;
+        }
+        return new TimePredicate[]{IsAfter.create(from), IsBefore.create(to)};
     }
 
-    public static TimePredicate[] isBetween(LocalDateTime start, LocalDateTime end, DateTimeType timeType) {
-        return new TimePredicate[]{IsAfter.create(timeType.down.apply(start)), IsBefore.create(timeType.down.apply(end))};
+    public static String parseTo(LocalDateTime target, String pattern) {
+        return target.format(getDateTimeFormatter(pattern));
     }
 
-
+    public static DateTimeFormatter getDateTimeFormatter(String pattern) {
+        DateTimeFormatter formatter = dateTimeFormatterCache.get(pattern);
+        if (formatter == null) {
+            synchronized (dateTimeFormatterCache) {
+                if (!dateTimeFormatterCache.containsKey(pattern)) {
+                    formatter = DateTimeFormatter.ofPattern(pattern);
+                    dateTimeFormatterCache.put(pattern, formatter);
+                } else {
+                    formatter = dateTimeFormatterCache.get(pattern);
+                }
+            }
+        }
+        return formatter;
+    }
 
     @FunctionalInterface
     interface BinaryTimePredicate extends BiPredicate<LocalDateTime, LocalDateTime> {
         BinaryTimePredicate IS_SAME_YEAR = ((source, target) -> source.getYear() == target.getYear());
-        BinaryTimePredicate IS_SAME_MONTH = ((source, target) -> source.getMonth() == target.getMonth());
-        BinaryTimePredicate IS_SAME_WEEK = ((source, target) -> source.getDayOfWeek() == target.getDayOfWeek());
-        BinaryTimePredicate IS_SAME_DAYS = ((source, target) -> source.getDayOfMonth() == target.getDayOfMonth());
-        BinaryTimePredicate IS_SAME_HOUR = ((source, target) -> source.getHour() == target.getHour());
-        BinaryTimePredicate IS_SAME_MIN = ((source, target) -> source.getMinute() == target.getMinute());
+        BinaryTimePredicate IS_SAME_MONTH = ((source, target) -> {
+            if (!IS_SAME_YEAR.test(source,target)) {
+                return false;
+            }
+            return source.getMonth() == target.getMonth();
+        });
+        BinaryTimePredicate IS_SAME_WEEK = ((source, target) -> {
+            if (!IS_SAME_YEAR.test(source,target)) {
+                return false;
+            }
+            return source.get(WEEK_FIELDS.weekOfWeekBasedYear()) == target.get(WEEK_FIELDS.weekOfWeekBasedYear());
+        });
+        BinaryTimePredicate IS_SAME_DAYS = ((source, target) -> {
+            if (!IS_SAME_YEAR.test(source,target)) {
+                return false;
+            }
+            return source.getDayOfYear() == target.getDayOfYear();
+        });
+        BinaryTimePredicate IS_SAME_HOUR = ((source, target) -> {
+            if (!IS_SAME_DAYS.test(source,target)) {
+                return false;
+            }
+            return source.getHour() == target.getHour();
+        });
+        BinaryTimePredicate IS_SAME_MIN = ((source, target) -> {
+            if (!IS_SAME_HOUR.test(source,target)) {
+                return false;
+            }
+            return source.getMinute() == target.getMinute();
+        });
         BinaryTimePredicate IS_BEFORE = LocalDateTime::isBefore;
         BinaryTimePredicate IS_AFTER = LocalDateTime::isAfter;
     }
